f7f6e9747c9665458e4479bf42068c674458811d
bellas-bytes <missisabellan@gmail.com>
2026-05-13 14:41:24 -0400

    update resume link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const GIF_SRC =
   "https://i.pinimg.com/originals/37/55/ce/3755ceb460354b7eb87f16cdb1ea0b59.gif";
 
 const RESUME_URL =
-  "https://drive.google.com/file/d/14_VFkO8inlRwbAh67Jt1dBOPa8iHMsgG/view?usp=sharing";
+  "https://drive.google.com/file/d/1wk-JEgFoY7eCf8HANBrzT8ei9SE1Dlcz/view?usp=sharing";
 
 function scrollToAbout() {
   document
